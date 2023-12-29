04e656dc140da5109b570345d0dd6eea3bef367d
ThogoGakinya <thogosamson@gmail.com>
2023-12-29 11:55:32 +0300

    DateCounter functionality. Dimistified different DateTime Methods

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class DateFunctions
+{
+    
+	public  void DaysLeft(DateTime fromDate, DateTime toDate)
+	{
+        int days = (int)toDate.Subtract(fromDate).TotalDays;
+        Console.WriteLine(days);
+	}
+    public static void Main()
+    {
+        DateFunctions men = new DateFunctions();
+        men.DaysLeft( DateTime.Now.Date.AddDays(-1), DateTime.Now);
+          
+    }
+}
